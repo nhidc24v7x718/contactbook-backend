b1abd6f3eba645b1e954c7c63ac1ce378c6b9a1a
nhidc24v7x718 <nhidc24v7x718@dttx.ctu.edu.vn>
2026-06-10 00:15:04 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
+});
+
+app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+  return next(new Error("404", "Resource not found"));
 });
 
 module.exports = app;
